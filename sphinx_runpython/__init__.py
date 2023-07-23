d030c834f251ca50e62cb067d0d52ba50c6e080b
Xavier Dupre <xadupre@microsoft.com>
2023-07-24 01:31:53 +0200

    action

--- sphinx_runpython/__init__.py
+++ sphinx_runpython/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.2.0"
 __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/sphinx-runpython"
-__url__ = "https://sdpython.github.io/doc/sphinx-runpython/index.html"
+__url__ = "https://sdpython.github.io/doc/sphinx-runpython/dev/"
 __license__ = "MIT License"
