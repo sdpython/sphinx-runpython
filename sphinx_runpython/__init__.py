3b29353fb6562528d1afc23dfad9b28ea840485c
xavier dupré <sdpython@users.noreply.github.com>
2023-09-14 00:08:18 +0200

    Update version to 0.3.0 (#26)
    * Update __init__.py
    
    * url
    
    ---------
    
    Co-authored-by: Xavier Dupre <xadupre@microsoft.com>

--- sphinx_runpython/__init__.py
+++ sphinx_runpython/__init__.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/sphinx-runpython"
 __url__ = "https://sdpython.github.io/doc/sphinx-runpython/dev/"
