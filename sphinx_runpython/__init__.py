bcd18b99e453e38c1d6e171bbc6492f70d403fa6
xadupre <xadupre@microsoft.com>
2025-05-27 10:41:59 +0200

    version

--- sphinx_runpython/__init__.py
+++ sphinx_runpython/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 __author__ = "Xavier Dupré"
 __github__ = "https://github.com/sdpython/sphinx-runpython"
 __url__ = "https://sdpython.github.io/doc/sphinx-runpython/dev/"
